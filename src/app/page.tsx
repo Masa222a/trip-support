cafaa4e883e8b7c5edc150e10e1901a167b98a54
Masa222a <guitarstring0716@gmail.com>
2025-08-24 14:43:07 +0900

    ファビコンを使用するにあたってHome画面に作成元を明記

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <FlightSearch />
+      <a className="mt-6" href="https://www.flaticon.com/free-icons/flight" title="flight icons">Flight icons created by vectorsmarket15 - Flaticon</a>
     </div>
   );
 }
